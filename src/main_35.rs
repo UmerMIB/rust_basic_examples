f2c97b3bd37986cd7d6bbea23460ea171b59cb47
UmerMIB <47888725+UmerMIB@users.noreply.github.com>
2019-08-31 16:52:37 +0500

    Update main_35.rs

--- src/main_35.rs
+++ src/main_35.rs
@@ -1,3 +1,36 @@
+// Project Calculator
+// Write a calculator program. A minimal calculator will support the following functions:
+// • numbers with decimals (not just integers)
+// • addition (1 + 2 is 3)
+// • subtraction (12 - 4 is 8)
+// • multiplication (33 * 2 is 66)
+// • division (3 / 8 is 0.375)
+// • exponents (2 ^ 3 is 8)
+// • error messages when you do something wrong
+// Your calculator should keep on running until explicitly told to quit. I suggest typing a zero as the first operand to
+// cause it to quit, i.e.
+// Program Console Sample:
+// >2 + 3
+// 5
+// >4 * 9
+// 36
+// >0 + 2
+// Bye, now.
+// Hint:
+// Well, if you read in everything as a String, then you can convert to other things.
+// What to avoid:
+// Any program, which presents me with a screen like the following, will not receive a very good score.
+// Program Console Sample:
+// Enter the function you wish to perform.
+// 1) addition
+// 2) subtraction
+// 3) multiplication
+// 4) division
+// 5) quit
+// Your choice:
+// Also, the same fate applies to any program that ever presents me with the following message:
+// Would you like to calculate again? (y/n) 
+
 
 fn main() {
     let mut flag: char = 'y';
